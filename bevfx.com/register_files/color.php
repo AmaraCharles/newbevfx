5c593625ce2a7bbad178476610d29147bef45cb9
AmaraCharles <falsepegasus@gmail.com>
2023-05-10 14:27:27 -1100

    first commit

---
+++ bevfx.com/register_files/color.php
@@ -0,0 +1,48 @@
+
+:root {
+    --main-color: #111111;
+}
+
+.feature-box .content {
+    box-shadow: inset 0 0px 66px #11111147;
+}
+
+.invest-plan-top::before {
+    box-shadow: inset 0 0px 66px #111111d4;
+}
+
+.invest-plan::before {
+    box-shadow: inset 0 0px 66px #1111113b;
+}
+
+.invest-plan-shape::after {
+    background-color: #11111133;
+}
+
+.invest-plan:hover .invest-plan-shape::after {
+    background-color: #111111;
+}
+
+.work-box {
+    box-shadow: inset 0 0px 20px #111111c4;
+}
+
+.work-box .icon {
+    box-shadow: inset 0 0px 20px #111111c4;
+}
+
+.cmn-table.style-separate tbody tr {
+    box-shadow: inset 0 0px 8px #1111114d;
+}
+
+.testimonial-box .content {
+    box-shadow: inset 0 0px 20px #111111c4;
+}
+
+.referral-box::before {
+    box-shadow: inset 0 0px 20px #111111c4;
+}
+
+.referral-box-step {
+    box-shadow: inset 0 0px 6px #1111119e;
+}
